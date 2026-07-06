c775831190f1fbf8a5eacf26e4a87a6c7423560b
adityathok <aditya0thok@gmail.com>
2026-07-06 14:27:18 +0700

    fix: perbaikan style floating-button

--- README.txt
+++ README.txt
@@ -4,7 +4,7 @@ Donate link: https://velocitydeveloper.com
 Tags: comments, spam
 Requires at least: 3.0.1
 Tested up to: 6.2
-Stable tag: 2.1.5
+Stable tag: 2.1.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- velocity-addons.php
+++ velocity-addons.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Velocity Addons
  * Plugin URI:        https://velocitydeveloper.com
  * Description:       Additional functionality for Velocitydeveloper clients
- * Version:           2.1.5
+ * Version:           2.1.6
  * Author:            Velocity
  * Author URI:        https://velocitydeveloper.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('VELOCITY_ADDONS_VERSION', '2.1.5');
+define('VELOCITY_ADDONS_VERSION', '2.1.6');
 define('VELOCITY_ADDONS_DB_VERSION', VELOCITY_ADDONS_VERSION);
 define('PLUGIN_DIR', plugin_dir_path(__DIR__));
 define('PLUGIN_FILE', plugin_basename(__FILE__));
